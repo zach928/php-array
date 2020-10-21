06f549b1a92a94a771bde39dd4098b176ea4e33c
zach928 <73217681+zach928@users.noreply.github.com>
2020-10-21 13:19:05 +0530

    Update php_array_example.php
    Solution of question 2 and 3

--- php_array_example.php
+++ php_array_example.php
@@ -10,6 +10,51 @@
    <?php
    $color = array('white', 'green', 'red', 'blue', 'black');
    echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the $color[2] carpet, the $color[1] lawn, the $color[0] house, the leaden sky. The new president and his first lady. - Richard M. Nixon"
-   and the words 'red', 'green' and 'white' will come from $color.
    ?>
 
+<!--
+    Q.2.
+    $color = array('white', 'green', 'red'') Go to the editor
+    Write a PHP script which will display the colors in the following way :
+    Output :
+    white, green, red,
+
+    green
+    red
+    white
+    -->
+<?php
+$color = array('white', 'green', 'red');
+sort($color);
+echo "<ul>";
+foreach($color as $y)
+        {
+            echo <li>$y</li>;
+        }    
+echo "</ul>";
+?>
+
+<!--
+    Q.3.
+         $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+
+        Create a PHP script which displays the capital and country name from the above array $ceu. Sort the list by the capital of the country. Go to the editor
+
+        Sample Output :
+        The capital of Netherlands is Amsterdam 
+        The capital of Greece is Athens 
+        The capital of Germany is Berlin 
+        - - - - - - - - - - - - - - - - - - - - - - - - -
+        - - - - - - - - - - - - - - - - - - - - - - - - -
+       -->
+
+<?php
+$ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+asort($ceu);
+echo "<ul>"
+foreach ($ceu as $country => $capital )
+        {
+            echo "<li>The capital of $country is $capital </li>";
+        }
+
+?>
