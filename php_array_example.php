d88e3b01b5d5518a979e1c112acad559ed9453e4
zach928 <73217681+zach928@users.noreply.github.com>
2020-10-21 12:47:43 +0530

    Update php_array_example.php
    Question about php indexed array

--- php_array_example.php
+++ php_array_example.php
@@ -1,1 +1,11 @@
-test
+ <!--
+     Q. 1. 
+     $color = array('white', 'green', 'red', 'blue', 'black');
+     Write a script which will display the following string - Go to the editor 
+    "The memory of that scene for me is like a frame of film forever frozen at that moment: the red carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon"
+    and the words 'red', 'green' and 'white' will come from $color.
+
+-->
+
+   
+
